64654834b747110bce6b5410df6a8bde430b7a15
skylark911 <54372844+skylark911@users.noreply.github.com>
2019-10-02 01:26:03 +0600

    Update fleaveme.py

--- stdplugins/fleaveme.py
+++ stdplugins/fleaveme.py
@@ -2,7 +2,7 @@
 
 Available Commands:
 
-.fleave"""
+.brb"""
 
 from telethon import events
 
@@ -26,7 +26,7 @@ async def _(event):
 
     input_str = event.pattern_match.group(1)
 
-    if input_str == "fleave":
+    if input_str == "brb":
 
         await event.edit(input_str)
 

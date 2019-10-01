85aa38c3ea7bc82ec1123fdd8472e1e5f89db5f0
skylark911 <54372844+skylark911@users.noreply.github.com>
2019-10-01 14:37:06 +0600

    Update ping.py

--- stdplugins/ping.py
+++ stdplugins/ping.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd(pattern="ping ?(.*)", allow_sudo=True))
+@borg.on(admin_cmd(pattern="ping"))
 async def _(event):
     if event.fwd_from:
         return
@@ -11,4 +11,4 @@ async def _(event):
     await event.edit("Pong!")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
-    await event.edit("Pong!\n{}".format(ms))
+    await event.edit("🏓Pong!\n{}".format(ms))

8ad25e35cae64178568802356c18a0c748d24fa6
skylark911 <54372844+skylark911@users.noreply.github.com>
2019-10-07 00:34:53 +0600

    Update ding.py

--- stdplugins/ding.py
+++ stdplugins/ding.py
@@ -2,7 +2,7 @@
 
 Available Commands:
 
-.ding"""
+.english"""
 
 from telethon import events
 
@@ -26,7 +26,7 @@ async def _(event):
 
     input_str = event.pattern_match.group(1)
 
-    if input_str == "ding":
+    if input_str == "english":
 
         await event.edit(input_str)
 

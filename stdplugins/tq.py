4f33fa4b6f2d6f492bbb5929b073042e5a460b45
skylark911 <54372844+skylark911@users.noreply.github.com>
2019-10-07 03:17:40 +0600

    Update tq.py

--- stdplugins/tq.py
+++ stdplugins/tq.py
@@ -14,7 +14,7 @@ from uniborg.util import admin_cmd
 async def _(event):
     if event.fwd_from:
         return
-    animation_interval = 0.1
+    animation_interval = 1.0
     animation_ttl = range(0, 90)
     input_str = event.pattern_match.group(1)
     if input_str == "tq":
@@ -25,25 +25,13 @@ async def _(event):
             "A",
             "N",
             "K",
-            "THANK",
             "Y",
             "O",
             "U",
-            "YOU",
-            "V",
-            "E",
-            "R",
-            "Y",
-            "VERY",
-            "M",
-            "U",
-            "C",
-            "H",
-            "MUCH"
-            "THANK YOU VERY MUCH 😇🌷"
+            "THANK YOU 😇🌷"
         ]
 
         for i in animation_ttl:
         	
             await asyncio.sleep(animation_interval)
-            await event.edit(animation_chars[i % 18])
+            await event.edit(animation_chars[i % 9])

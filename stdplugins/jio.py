c0a72eac969c8f115f95d20c804d2416e1e80580
skylark911 <54372844+skylark911@users.noreply.github.com>
2019-10-09 15:44:33 +0600

    Update jio.py

--- stdplugins/jio.py
+++ stdplugins/jio.py
@@ -12,7 +12,7 @@ import asyncio
 
 
 
-@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.(.*)", allow_sudo=True, outgoing=True))
 
 async def _(event):
 
